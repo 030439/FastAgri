16e3ecd6de58a5c089cd31eabf40e11e54b0b9a4
shmas khan <sktanoli420@gmail.com>
2024-02-23 18:26:24 +0500

    final production tab

--- application/views/pages/customer/add-customer.php
+++ application/views/pages/customer/add-customer.php
@@ -14,10 +14,10 @@
 
         <div class="lg:col-span-4 md:col-span-6 col-span-12">
             <div class="cashier-select-field mb-5">
-                <h5 class="text-[15px] text-heading font-semibold mb-3">Product</h5>
+                <h5 class="text-[15px] text-heading font-semibold mb-3">Company</h5>
                 <div class="cashier-input-field-style">
                     <div class="single-input-field w-full">
-                        <input type="text" placeholder="product">
+                        <input type="text" placeholder="Company">
                     </div>
                 </div>
             </div>
@@ -26,55 +26,26 @@
 
         <div class="lg:col-span-4 md:col-span-6 col-span-12">
             <div class="cashier-select-field mb-5">
-                <h5 class="text-[15px] text-heading font-semibold mb-3">Transport Charges</h5>
+                <h5 class="text-[15px] text-heading font-semibold mb-3">Contact</h5>
                 <div class="cashier-input-field-style">
                     <div class="single-input-field w-full">
-                        <input type="text" placeholder="unit">
+                        <input type="text" placeholder="Contact">
                     </div>
                 </div>
             </div>
         </div>
         <div class="lg:col-span-4 md:col-span-6 col-span-12">
             <div class="cashier-select-field mb-5">
-                <h5 class="text-[15px] text-heading font-semibold mb-3">Quantity</h5>
+                <h5 class="text-[15px] text-heading font-semibold mb-3">Address</h5>
                 <div class="cashier-input-field-style">
                     <div class="single-input-field w-full">
-                        <input type="text" placeholder="Quantity">
+                        <input type="text" placeholder="Address">
                     </div>
                 </div>
             </div>
         </div>
-        <div class="lg:col-span-4 md:col-span-6 col-span-12">
-            <div class="cashier-select-field mb-5">
-                <h5 class="text-[15px] text-heading font-semibold mb-3">Rate</h5>
-                <div class="cashier-input-field-style">
-                    <div class="single-input-field w-full">
-                        <input type="text" placeholder="Rate">
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="lg:col-span-4 md:col-span-6 col-span-12">
-            <div class="cashier-select-field mb-5">
-                <h5 class="text-[15px] text-heading font-semibold mb-3">Name</h5>
-                <div class="cashier-select-field-style">
-                    <select class="block" style="display: none;">
-                        <option selected="" disabled="" value="default">Select Vendor</option>
-                        <option value="language-1">Jonathan Deo</option>
-                        <option value="language-2">Andrew Tye</option>
-                        <option value="language-3">Peter Parkar</option>
-                    </select>
-                    <div class="nice-select block" tabindex="0"><span class="current">Select Vendor</span>
-                        <ul class="list">
-                            <li data-value="default" class="option selected disabled focus">Select Vendor</li>
-                            <li data-value="language-1" class="option">Jonathan Deo</li>
-                            <li data-value="language-2" class="option">Andrew Tye</li>
-                            <li data-value="language-3" class="option">Peter Parkar</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+
 
 
 
