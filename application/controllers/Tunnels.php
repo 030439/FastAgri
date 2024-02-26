d6b91ada58873cf4fbc43f5893a48a16cd8b5360
shmas khan <sktanoli420@gmail.com>
2024-02-26 14:40:18 +0500

    load trait

--- application/controllers/Tunnels.php
+++ application/controllers/Tunnels.php
@@ -3,7 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 class Tunnels extends CI_Controller{
-    
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('mytrait');
+    }
+
     public function index(){
         $this->load->view('layout/parts',['page'=>"pages/tunnels/list-tunnel"]);
     }
---
+++ system/libraries/MyTrait.php
@@ -0,0 +1,8 @@
+<?php
+// application/libraries/MyTrait.php
+
+class MyTrait {
+    public function traitFunction() {
+        // Your trait-like functionality here
+    }
+}
