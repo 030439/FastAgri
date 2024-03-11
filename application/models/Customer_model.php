6a26c809aabdd514ee62f1ebb376587395a9a6e3
030439 <praimchand763@gmail.com>
2024-03-10 17:04:40 -0700

    customer model

---
+++ application/models/Customer_model.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Customer_model extends CI_Model {
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+        $this->load->library('form_validation');
+    }
+    public function getshareholders() {
+        $shareholders = $this->db->get('shareholders')->result();
+        return $shareholders;
+    }
+
+    public function createCustomer($data) {
+        return $this->db->insert('customers', $data);
+    }
+
+    public function getshareholderById($id) {
+        $shareholder = $this->db->get_where('shareholders', ['id' => $id])->row();
+        return $shareholder;
+    }
+
+    public function updateShareHolder($id, $data) {
+      $this->db->where('id', $id);
+       return  $this->db->update('shareholders', $data);
+    }
+
+    public function deleteshareholder($id) {
+        return $this->db->delete('shareholders', ['id' => $id]);
+    }
+}
