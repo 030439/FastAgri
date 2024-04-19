63545c51a3161b5e55ee95a72bf24b4df5039ae7
030439 <praimchand763@gmail.com>
2024-04-19 12:24:24 -0700

    issue stock page working

--- application/controllers/Stock.php
+++ application/controllers/Stock.php
@@ -130,8 +130,7 @@ class Stock extends CI_Controller {
 	}
 	public function listissue()
 	{
-		$data['tunnels']=$this->Stock_model->issueList();
-		dd($data);
+		$data['stock']=$this->Stock_model->issueList();
 		$this->load->view('layout/parts',['page'=>"pages/stock/list-issue-stock",'data'=>$data]);
 	}
 	public function getStockRate(){
