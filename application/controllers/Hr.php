28c7fa4039d5e111a17e25abe7f9db4b5fb1129b
030439 <praimchand763@gmail.com>
2024-04-19 13:15:59 -0700

    abour-rate page

--- application/controllers/Hr.php
+++ application/controllers/Hr.php
@@ -3,12 +3,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Hr extends CI_Controller {
 
-	
+	public function __construct() {
+        parent::__construct();
+		$this->load->model('labour_model');
+		$this->load->model('Setup_model');
+        $this->load->library('form_validation');
+    }
+
 	public function index()
 	{
 		$this->load->view('layout/parts',['page'=>"pages/human-resource/list-employee"]);
 	}
-	
+	public function labourRate(){
+		try{
+		    $this->load->view('layout/parts',['page'=>"pages/human-resource/labour-rate"]);
+	    } catch (Exception $e) {
+			log_message('error', $e->getMessage());
+			show_error('An unexpected error occurred. Please try again later.');
+	   }
+	}
 	public function add()
 	{
 		
