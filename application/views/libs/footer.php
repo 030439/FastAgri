47f88b06af539516419a20fe45d5b0b1978761f0
030439 <praimchand763@gmail.com>
2024-06-27 12:25:13 +0500

    button done

--- application/views/libs/footer.php
+++ application/views/libs/footer.php
@@ -35,11 +35,12 @@
  <script src="assets/js/main.js"></script>
  <script src="assets/js/datatable.js"></script>
  <script src="assets/js/toaster.js"></script>
- 
-<script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+ <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
-
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
 
  <script>
  
