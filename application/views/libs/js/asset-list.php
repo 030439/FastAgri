67cbd7a90c41c9905636cd1a174fdbfcfcc85535
030439 <praimchand763@gmail.com>
2024-07-13 02:14:51 +0500

    asset edit operation

--- application/views/libs/js/asset-list.php
+++ application/views/libs/js/asset-list.php
@@ -19,15 +19,9 @@ $('#user-list').DataTable({
             { "data": "cDate" },
             
             { 
-                "data": "status",
+                "data": "id",
                 "render": function(data, type, row) {
-                    console.log(data);
-                    if(data==1){
-                        statusName="Active";
-                    }else{
-                        statusName="Close";
-                    }
-                    return '<button style="background-color:#86af49;padding:3px 5px;color:#fff" class="btn btn-primary">'+statusName+'</button> ';
+                    return '<a style="background-color:#86af49;padding:3px 5px;color:#fff" href="asset-edit/'+data+'" class="btn btn-primary">Update</a> ';
                 }
             }, 
             { 
@@ -41,44 +35,8 @@ $('#user-list').DataTable({
       });
       
         
-        function profitButtonClick(id) {
-            window.location.href="tunnel/tunnle-profit/"+id
-        }
+       
 
-        // Get the modal
-        var modal = document.getElementById("myModal");
+        
 
-        // Get the button that opens the modal
-        var btn = document.getElementByClass("btn-primary");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks on the button, open the modal
-        btn.onclick = function() {
-        modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-        modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-        }
-        function handleButtonClick(id) {
-            $.ajax({
-                url:"getAssetShares",
-                method:"post",
-                data:{id:id},
-                success:function(e){
-                    $("#tbody-display").html(e);
-                }
-            })
-            modal.style.display = "block";
-        }
     </script>
--- application/views/pages/asset/list.php
+++ application/views/pages/asset/list.php
@@ -14,7 +14,6 @@
         <div class="cashier-salereturns-table-area">
             <div class="cashier-salereturns-table-area">
                 <div class="cashier-salereturns-table-innerC">
-                <button id="myBtn">Open Modal</button>
                     <table id="user-list"  class="table table-bordered borderd table-striped display table-hover">
                         <thead>
                             <tr>
@@ -30,44 +29,4 @@
             </div>
     </div>
 </div>
-<style>
-.modal {
-  display: none; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  padding-top: 100px; /* Location of the box */
-  left: 1;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
-/* Modal Content */
-.modal-content {
-  background-color: #fefefe;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 40%;
-}
-/* The Close Button */
-.close {
-  color: #aaaaaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-.close:hover,
-.close:focus {
-  color: #000;
-  text-decoration: none;
-  cursor: pointer;
-}
-th ,td{
-    width:33%;
-    text-align:center;
-}
-</style>
 <?php $file="asset-list.php";?>
